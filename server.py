6059802e8000e9aa44e576ebada461a34f132c74
Adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-04-08 16:58:53 +0200

    Final project first commit (just initial code and creating files)

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
-#FINAL PROJECT SERVER
+#SERVER CODE: FINAL PRACTICE#
+#First things first; we import the libraries needed#
+
+import http.server
+import termcolor
+import socketserver
+
+
+#Next step is defining the port we're going to use#
+PORT = 8000
+socketserver.TCPServer.allow_reuse_adress = True #this code line allow us to reuse the same port over and over again#
+
+#IN order to continue, we must create a class for our server related to the http protocol#
+class TestHandler(http.server.BaseHTTPRequestHandler):
+    #get function
+
+
