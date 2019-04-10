b0bc1127b6666b6676e6fe0619dee04ba6c2747f
Adriespi <a.espinosag.2018@alumnos.urjc.es>
2019-04-10 20:01:38 +0200

    final project update

--- server.py
+++ server.py
@@ -12,6 +12,24 @@ socketserver.TCPServer.allow_reuse_adress = True #this code line allow us to reu
 
 #IN order to continue, we must create a class for our server related to the http protocol#
 class TestHandler(http.server.BaseHTTPRequestHandler):
-    #get function
+    #get function: access to the get method in the http protocol request
+    def get_method(self):
+        termcolor.cprint(self.requestline,'blue')#first line
+        #stablish the path to follow(.path)
+        meth_list = self.path.split('?')
+        meth = meth_list[0]
+        if meth == '/':
+            j = open('indexfinalpractice.html','r')#open the html file to use it
+            info = j.read()
+            selectinfo = 'text/html'
+            code = 200
+        elif meth == '/karyotype':#the use selects the karyotype endpoint
+            server = 'http://rest.ensembl.org'#api rest endpoint
+            dir = '/info/assembly/'
+            #we put the hole karyotype extraction info in a try just in case the user does not enter one of the index options
+            try:
+
+
+
 
 
